feea4384dd3a2caa323007315cd5d7de0527f286
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2022-02-13 16:35:33 +0530

    remove bootstrap add tailwind back

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 export default function Login() {
   return (
     <>
-      <Navbar />
-      Login
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
