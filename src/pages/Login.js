43f5c5ebd4e5820b7fc49519053a1c1ea0827dec
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2022-02-13 22:22:42 +0530

    add footer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Login() {
   return (
-    <div className="custom-login-bg">
+    <div className="custom-login-bg relative ">
       <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Footer />
     </div>
   );
 }
