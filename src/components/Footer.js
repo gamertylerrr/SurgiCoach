e660690c3805ea7bdb5ebdeb69f1ac6117bd4570
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2022-02-15 00:13:14 +0530

    init project to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer className="absolute inset-x-0 bottom-0 w-64  mx-auto my-4 text-center">
-      <p className="m-0 text-sm">Copyright ChonaMedTech, LLC</p>
+      {window.location.pathname === '/signup' ? (
+        <p className="m-0 text-sm">
+          By clicking ”Register”, I agree to terms of use
+          <Link className="underline">(legal disclaimer Here)</Link>
+        </p>
+      ) : (
+        <p className="m-0 text-sm">Copyright ChonaMedTech, LLC</p>
+      )}
     </footer>
   );
 }
