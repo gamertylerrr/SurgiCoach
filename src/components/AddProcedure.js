642dec7933fc14dfbe360509f2693cd4133e3a21
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2022-02-16 00:46:28 +0530

    my procedure desktop view done

---
+++ src/components/AddProcedure.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default function AddProcedure() {
+  return (
+    <div className="text-center mx-auto w-11/12">
+      <p className="font-black text-xl uppercase mb-6">Add new procedure</p>
+      <form action="">
+        <div className="flex flex-col items-center justify-between">
+          <input
+            type="text"
+            className="custom-input m-2 px-6 py-2"
+            placeholder="procedure name"
+          />
+          <input
+            type="text"
+            className="custom-input m-2 px-6 py-2"
+            placeholder="message type"
+          />
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <input
+            type="text"
+            className="custom-input my-2 mr-4  py-2"
+            placeholder="days"
+            style={{ maxWidth: '100px' }}
+          />
+          <input
+            type="text"
+            className="custom-input my-2  px-6 py-2"
+            placeholder="before procedure"
+          />
+        </div>
+        <div className="custom-input my-2 px-6 py-2">
+          <label for="files" class="btn">
+            UPLOAD FILE (OPTIONAL)
+          </label>
+          <input id="files" className="hidden" type="file" />
+        </div>
+        <div className="flex flex-col items-center justify-between">
+          <input
+            type="text"
+            className="custom-input m-2 px-6 py-2"
+            placeholder="Add text (optional)"
+          />
+        </div>
+        <div className="">
+          <button
+            type="submit"
+            className="custom-btn text-white my-2 px-6 py-2 uppercase"
+          >
+            submit message
+          </button>
+          <button
+            type="submit"
+            className="custom-btn text-white my-2 px-6 py-2 uppercase"
+          >
+            add message
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/pages/MyProcedure.js
+++ src/pages/MyProcedure.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddProcedure from '../components/AddProcedure';
 import Header from '../components/Header';
 import Procedure from '../components/Procedure';
 
@@ -27,23 +28,58 @@ const sampleData = [
   {
     name: 'labrum',
   },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
+  {
+    name: 'labrum',
+  },
 ];
 
 export default function MyProcedure() {
   return (
     <div className="custom-login-bg">
       <Header />
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="text-center">
+      <div className="container mx-auto w-4/5 md:w-11/12 lg:w-45 ">
+        <div className="grid grid-cols-1 md:grid-cols-3 ">
+          <div className="text-center col-span-2 lg:px-6 lg:mr-8">
             <p className="font-black text-xl uppercase mb-8">My Procedure</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 scroll-menu md:pr-4 lg:pr-8">
               {sampleData.map((data, index) => (
                 <Procedure data={data} number={index + 1} />
               ))}
             </div>
           </div>
-          <div></div>
+          <div className="hidden md:block">
+            <AddProcedure />
+          </div>
         </div>
       </div>
     </div>
