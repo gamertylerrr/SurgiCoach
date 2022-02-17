bc2c8e74356f96e61626cba0308e511d618da3fe
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2022-02-17 16:53:49 +0530

    add edit and delete btn in procedures

--- src/components/Procedure.js
+++ src/components/Procedure.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 export default function Procedure({ data, number }) {
   return (
-    <div className="procedure-wrapper flex flex-row">
-      <div class="basis-1/6 procedure-number p-2 ">
-        <p className="text-black text-center">{number}</p>
+    <div>
+      <div className="procedure-wrapper flex flex-row">
+        <div class="basis-1/6 procedure-number p-2 ">
+          <p className="text-black text-center">{number}</p>
+        </div>
+        <div class="basis-5/6 p-2">
+          <p className="text-center uppercase">{data.name}</p>
+        </div>
       </div>
-      <div class="basis-5/6 p-2">
-        <p className="text-center uppercase">{data.name}</p>
+      <div className="flex flex-row justify-center items-center my-2">
+        <button className="secondary-btn py-0 px-6 mx-2 text-center w-24 text-white">
+          Edit
+        </button>
+        <button className="secondary-btn py-0 px-6 mx-2 text-center w-24 text-white">
+          Delete
+        </button>
       </div>
     </div>
   );
