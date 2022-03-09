62266ede32faa48a69292818d9a2cd35675a889a
shashwat0802 <shashwatgupta4656@gmail.com>
2022-03-10 01:09:55 +0530

    small email change

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(functions.config().sendgrid.key);
 
 exports.welcomeEmail = functions.auth.user().onCreate((user) => {
   const msg = {
-    to: 'shashwatgupta4656@gmail.com',
+    to: 'surgicoach@gmail.com',
     from: 'surgicoach@gmail.com',
     templateId: 'd-b85d5ff3438e4c548cd9588dccc2422e',
     dynamic_template_data: {
