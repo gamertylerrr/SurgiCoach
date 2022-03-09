7ec396e7ed9528b6580f2c24cd2d076df2f314fb
shashwat0802 <shashwatgupta4656@gmail.com>
2022-03-10 00:31:04 +0530

    test email function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.tests = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+sgMail.setApiKey(functions.config().sendgrid.key);
+
+export const welcomeEmail = functions.auth.onCreate((user) => {
+  const msg = {
+    to: 'shashwatgupta4656@gmail.com',
+    // from: 'surgicoach@gmail.com',
+    templateId: 'd-b85d5ff3438e4c548cd9588dccc2422e',
+    dynamic_template_data: {
+      email: user.email,
+    },
+  };
+  return sgMail.send(msg);
 });
