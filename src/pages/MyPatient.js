87c56ba887d4dd5a60cd340434c633d06934c2a3
shashwat0802 <shashwatgupta4656@gmail.com>
2022-03-12 03:53:56 +0530

    small changes

--- src/pages/MyPatient.js
+++ src/pages/MyPatient.js
@@ -68,7 +68,7 @@ export default function MyPatient() {
   return (
     <div className="custom-login-bg relative ">
       <Header />
-      <div className="container mx-auto w-4/5 md:w-11/12 lg:w-45 mt-12">
+      <div className="container mx-auto md:w-11/12 lg:w-45 mt-12 p-4">
         <div className="grid grid-cols-1 md:grid-cols-5 md:gap-12 ">
           <div className="col-span-2">
             <p className="font-black text-xl uppercase ">My patients</p>
